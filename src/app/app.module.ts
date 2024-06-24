fb2d488ae39cc20bcc80dfd55442b57f08eef785
Gust4v0M <gustavomorgado6@gmail.com>
2024-06-24 17:26:49 -0300

    colocando icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TarefasComponent } from './tarefas/tarefas.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TarefasComponent } from './tarefas/tarefas.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
--- src/app/tarefas/tarefas.component.ts
+++ src/app/tarefas/tarefas.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { FormsModule,  } from '@angular/forms';
 
 @Component({
   selector: 'app-tarefas',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './tarefas.component.css'
 })
 export class TarefasComponent {
+inputValue: string ='';
+tasks: string[] = [];
+trashIcon = '../assets/icons/image.png'
+constructor(private forms: FormsModule){
 
+}
+
+addTask(){
+  this.tasks.push(this.inputValue);
+}
+
+removeTask(task: string){
+  const index = this.tasks.indexOf(task)
+ if(index > -1){
+  this.tasks.splice(index, 1)
+ }
+}
 }
